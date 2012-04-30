dc4c52434c47722e7a97602efdb9318ca70bc697
Dave <dave@spudooli.com>
2012-04-30 21:39:27 +1200

    Added in the ino

---
+++ martino.ino
@@ -0,0 +1,228 @@
+/*
+  Example code for the Freetronics LCD & Keypad Shield:
+  
+    http://www.freetronics.com/products/lcd-keypad-shield
+  
+  by Marc Alexander, 7 September 2011
+  This example code is in the public domain.
+  
+*/
+
+#include <LiquidCrystal.h>  
+
+// Pins in use
+#define BUTTON_ADC_PIN           A0  // A0 is the button ADC input
+#define LCD_BACKLIGHT_PIN         3  // D3 controls LCD backlight
+// ADC readings expected for the 5 buttons on the ADC input
+#define RIGHT_10BIT_ADC           0  // right
+#define UP_10BIT_ADC            145  // up
+#define DOWN_10BIT_ADC          329  // down
+#define LEFT_10BIT_ADC          505  // left
+#define SELECT_10BIT_ADC        741  // right
+#define BUTTONHYSTERESIS         10  // hysteresis for valid button sensing window
+//return values for ReadButtons()
+#define BUTTON_NONE               0  // 
+#define BUTTON_RIGHT              1  // 
+#define BUTTON_UP                 2  // 
+#define BUTTON_DOWN               3  // 
+#define BUTTON_LEFT               4  // 
+#define BUTTON_SELECT             5  // 
+//some example macros with friendly labels for LCD backlight/pin control, tested and can be swapped into the example code as you like
+#define LCD_BACKLIGHT_OFF()     digitalWrite( LCD_BACKLIGHT_PIN, LOW )
+#define LCD_BACKLIGHT_ON()      digitalWrite( LCD_BACKLIGHT_PIN, HIGH )
+#define LCD_BACKLIGHT(state)    { if( state ){digitalWrite( LCD_BACKLIGHT_PIN, HIGH );}else{digitalWrite( LCD_BACKLIGHT_PIN, LOW );} }
+
+byte buttonJustPressed  = false;         //this will be true after a ReadButtons() call if triggered
+byte buttonJustReleased = false;         //this will be true after a ReadButtons() call if triggered
+byte buttonWas          = BUTTON_NONE;   //used by ReadButtons() for detection of button events
+
+
+LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );   //Pins for the freetronics 16x2 LCD shield. LCD: ( RS, E, LCD-D4, LCD-D5, LCD-D6, LCD-D7 )
+
+unsigned char relayPin[4] = {10, 11, 12, 13};
+
+void martini()
+  {
+   lcd.setCursor( 0, 0 );   
+   lcd.print( "Right, 1 Martini" );
+   lcd.setCursor( 0, 1 );   
+   //          1234567890123456
+   lcd.print( "  coming up     " );
+   delay(2000);
+   vermouth(5000);
+   delay(2000);
+   gin(5500);
+   Serial.println("Just made a Martini");
+   delay(2000);
+   lcdreadymode();   
+   }
+  
+void vermouth(int timeamount)
+  {
+    Serial.println("Doing the Vermouth bit");
+    digitalWrite( 10, HIGH);
+    delay(timeamount);
+    digitalWrite( 10, LOW);
+  }
+
+void gin(int timeamount)
+  {
+    Serial.println("Now the gin");
+    digitalWrite( 11, HIGH);
+    delay(timeamount);
+    digitalWrite( 11, LOW);
+  }
+  
+void lcdreadymode()
+  {
+    lcd.setCursor( 0, 0 );   //top left
+    //          1234567890123456
+    lcd.print( "A Martini Dave? " );
+    //
+    lcd.setCursor( 0, 1 );   //bottom left
+    //          1234567890123456
+    lcd.print( "Press Select -->" );
+  }
+  
+void setup()
+{
+   //button adc input
+   pinMode( BUTTON_ADC_PIN, INPUT );         //ensure A0 is an input
+   digitalWrite( BUTTON_ADC_PIN, LOW );      //ensure pullup is off on A0
+   //lcd backlight control
+   digitalWrite( LCD_BACKLIGHT_PIN, HIGH );  //backlight control pin D3 is high (on)
+   pinMode( LCD_BACKLIGHT_PIN, OUTPUT );     //D3 is an output
+   //set up the LCD number of columns and rows: 
+   lcd.begin( 16, 2 );
+
+   lcdreadymode();
+   
+   
+   //Set the relay output pins
+   pinMode(10, OUTPUT);
+   digitalWrite( 10, LOW);
+   pinMode(11, OUTPUT);
+   digitalWrite( 11, LOW);
+   pinMode(12, OUTPUT);
+   digitalWrite( 12, LOW);
+   pinMode(13, OUTPUT);
+   digitalWrite( 13, LOW);
+   
+   Serial.begin(9600);
+   Serial.println("Powered Up");
+}
+
+void loop()
+{
+   byte button;
+   byte timestamp;
+   
+   //get the latest button pressed, also the buttonJustPressed, buttonJustReleased flags
+   button = ReadButtons();
+   //blank the demo text line if a new button is pressed or released, ready for a new label to be written
+   if( buttonJustPressed || buttonJustReleased )
+   {
+     //lcd.setCursor( 4, 1 );
+     //lcd.print( "            " );
+   }
+   //show text label for the button pressed
+   switch( button )
+   {
+      case BUTTON_NONE:
+      {
+         break;
+      }
+      case BUTTON_RIGHT:
+      {
+         lcdreadymode();
+         break;
+      }
+      case BUTTON_UP:
+      {
+         LCD_BACKLIGHT_OFF();
+         break;
+      }
+      case BUTTON_DOWN:
+      {
+         LCD_BACKLIGHT_ON(); 
+         break;
+      }
+      case BUTTON_LEFT:
+      {
+
+        break;
+     }
+     case BUTTON_SELECT:
+     {
+        martini();
+          
+        break;
+      }
+      default:
+     {
+        break;
+     }
+   }
+
+   //clear the buttonJustPressed or buttonJustReleased flags, they've already done their job now.
+   if( buttonJustPressed )
+      buttonJustPressed = false;
+   if( buttonJustReleased )
+      buttonJustReleased = false;
+}
+/*--------------------------------------------------------------------------------------
+  ReadButtons()
+  Detect the button pressed and return the value
+  Uses global values buttonWas, buttonJustPressed, buttonJustReleased.
+--------------------------------------------------------------------------------------*/
+byte ReadButtons()
+{
+   unsigned int buttonVoltage;
+   byte button = BUTTON_NONE;   // return no button pressed if the below checks don't write to btn
+   
+   //read the button ADC pin voltage
+   buttonVoltage = analogRead( BUTTON_ADC_PIN );
+   //sense if the voltage falls within valid voltage windows
+   if( buttonVoltage < ( RIGHT_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_RIGHT;
+   }
+   else if(   buttonVoltage >= ( UP_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( UP_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_UP;
+   }
+   else if(   buttonVoltage >= ( DOWN_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( DOWN_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_DOWN;
+   }
+   else if(   buttonVoltage >= ( LEFT_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( LEFT_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_LEFT;
+   }
+   else if(   buttonVoltage >= ( SELECT_10BIT_ADC - BUTTONHYSTERESIS )
+           && buttonVoltage <= ( SELECT_10BIT_ADC + BUTTONHYSTERESIS ) )
+   {
+      button = BUTTON_SELECT;
+   }
+   //handle button flags for just pressed and just released events
+   if( ( buttonWas == BUTTON_NONE ) && ( button != BUTTON_NONE ) )
+   {
+      //the button was just pressed, set buttonJustPressed, this can optionally be used to trigger a once-off action for a button press event
+      //it's the duty of the receiver to clear these flags if it wants to detect a new button change event
+      buttonJustPressed  = true;
+      buttonJustReleased = false;
+   }
+   if( ( buttonWas != BUTTON_NONE ) && ( button == BUTTON_NONE ) )
+   {
+      buttonJustPressed  = false;
+      buttonJustReleased = true;
+   }
+   
+   //save the latest button value, for change event detection next time round
+   buttonWas = button;
+   
+   return( button );
+}
